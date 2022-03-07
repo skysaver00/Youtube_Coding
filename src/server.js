fe7dcc73d6db09449ae1958dbe2b3e92fdc73eaf
ywkim <skysaver00@naver.com>
2022-03-07 16:45:57 +0900

    #3.2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
-const app = express();
+const app = express(); //application 생성
+
+const handleHome = () => console.log("Somebody is trying to go home.")
+app.get("/", handleHome) //누군가 /으로 request를 보내면, callback해주겠다는 뜻.
 
 const PORT = 4000
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`)
+const handleListening = () => console.log(`✅Server listening on port http://localhost:${PORT} 🚀`)
 
-app.listen(PORT, handleListening) //뒤에가 callback
+app.listen(PORT, handleListening) //뒤에가 callback. listen 생성
