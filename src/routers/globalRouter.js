fda4aacc6832368f9c03741ce9f06686d34e878b
ywkim <skysaver00@naver.com>
2022-03-10 15:46:41 +0900

    #4.3

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { join } from "../controllers/userController";
+import { trending } from "../controllers/videoController";
 
 const globalRouter = express.Router(); //라우터 생성
 const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
 
-export default globalRouter
+globalRouter.get("/", handleHome);
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
+
+export default globalRouter;
