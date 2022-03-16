f15a38d9e884eabf34a366a89f8d8c93f579563b
ywkim <skysaver00@naver.com>
2022-03-16 16:32:25 +0900

    #4.6

---
+++ URL.md
@@ -0,0 +1,16 @@
+# Youtube_Coding 유튜브 클론 코딩
+
+/ -> Home   
+/join -> Join   
+/login -> Login   
+/search -> Search  
+      
+/users/:id -> See User   
+/users/logout -> Log Out   
+/users/edit -> Edit MY Profile   
+/users/delete -> Delete MY Profile  
+      
+/videos/:id -> See Video   
+/videos/:id/edit -> Edit Video   
+/videos/:id/delete -> Delete Video   
+/videos/upload -> Upload Video  
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { join } from "../controllers/userController";
-import { trending } from "../controllers/videoController";
+import { join, login } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router(); //라우터 생성
 const handleHome = (req, res) => res.send("Home");
 
-globalRouter.get("/", handleHome);
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
