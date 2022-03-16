69967631a1b98a83f4f7ba6193bce10f4f94e49a
ywkim <skysaver00@naver.com>
2022-03-16 16:48:56 +0900

    #4.7

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,7 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
+export const see = (req, res) => {
+    return res.send(`Watch Video #${req.params.id}`);
+}
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
